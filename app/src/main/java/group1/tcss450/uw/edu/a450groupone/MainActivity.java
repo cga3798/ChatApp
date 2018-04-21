1d83391c2b24766b2cb366708273c97d7a336feb
casey anderson <cga3798@uw.edu>
2018-04-20 22:23:24 -0700

    finished log in fragment look

--- app/src/main/java/group1/tcss450/uw/edu/a450groupone/MainActivity.java
+++ app/src/main/java/group1/tcss450/uw/edu/a450groupone/MainActivity.java
@@ -3,11 +3,30 @@ package group1.tcss450.uw.edu.a450groupone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import group1.tcss450.uw.edu.a450groupone.model.Credentials;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnLoginFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null) {
+            if (findViewById(R.id.fragmentContainer) != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragmentContainer, new LoginFragment())
+                        .commit();
+            }
+        }
+    }
+
+    @Override
+    public void onLogin(Credentials creds) {
+
+    }
+
+    @Override
+    public void onRegister() {
+
     }
 }
