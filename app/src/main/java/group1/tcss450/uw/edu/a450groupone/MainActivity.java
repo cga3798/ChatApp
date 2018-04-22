37f2a86559b8b202f7a5e2c2e9817ffc8433d215
Kamalpreet Singh <kamalpreetus@gmail.com>
2018-04-21 21:11:25 -0700

    Added weather screen.

--- app/src/main/java/group1/tcss450/uw/edu/a450groupone/MainActivity.java
+++ app/src/main/java/group1/tcss450/uw/edu/a450groupone/MainActivity.java
@@ -2,12 +2,28 @@ package group1.tcss450.uw.edu.a450groupone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WeatherFragment.OnWeatherFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            if (findViewById(R.id.fragmentContainer) != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragmentContainer, new WeatherFragment())
+                        .commit();
+            }
+        }
+
+        Log.d("MARS", "inside oncreate.");
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+
     }
 }
