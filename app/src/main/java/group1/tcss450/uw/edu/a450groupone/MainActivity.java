76a8a70dd37ec905344a2c6ace382cbedfa1091c
Kamalpreet Singh <kamalpreetus@gmail.com>
2018-04-23 10:11:34 -0700

    Cleaned up MainActivity.

--- app/src/main/java/group1/tcss450/uw/edu/a450groupone/MainActivity.java
+++ app/src/main/java/group1/tcss450/uw/edu/a450groupone/MainActivity.java
@@ -5,27 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements ChatFragment.OnChatFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if(savedInstanceState == null) {
-            if (findViewById(R.id.fragmentContainer) != null) {
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragmentContainer, new ChatFragment())
-                        .commit();
-            }
-        }
-
-        Log.d("MARS", "inside oncreate.");
-    }
-
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
     }
 }
