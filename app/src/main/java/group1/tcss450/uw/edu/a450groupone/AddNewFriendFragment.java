b1157c5ab6e2ce292d59b9099d3c6427a0e7f504
HasnahSaid <hasna024@uw.edu>
2018-05-05 20:13:57 -0700

    navigation menu icons and color

--- app/src/main/java/group1/tcss450/uw/edu/a450groupone/AddNewFriendFragment.java
+++ app/src/main/java/group1/tcss450/uw/edu/a450groupone/AddNewFriendFragment.java
@@ -19,6 +19,7 @@ public class AddNewFriendFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //friend frag
         View v = inflater.inflate(R.layout.fragment_add_new_friend, container, false);
         return v;
     }
