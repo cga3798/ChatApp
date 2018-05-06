48ab25d8c04b24828ae5b692dd1d28e191341c0e
HasnahSaid <hasna024@uw.edu>
2018-05-05 20:10:59 -0700

    navigation menu color&icons

---
+++ app/src/main/java/group1/tcss450/uw/edu/a450groupone/AddNewFriendFragment.java
@@ -0,0 +1,26 @@
+package group1.tcss450.uw.edu.a450groupone;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AddNewFriendFragment extends Fragment {
+
+
+    public AddNewFriendFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_add_new_friend, container, false);
+        return v;
+    }
+
+}
